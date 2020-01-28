3e5a63febd78ecbe234fe210b33d26bb9ed37370
Martin Gustavsson <Marreeeehh93@gmail.com>
2020-01-28 11:40:33 +0100

    added form html and css

--- app.js
+++ app.js
@@ -5,14 +5,3 @@
 
 
 // Fetching from API
-const getRandomRecipe = async () => {
-    const response = await fetch(`
-};
-
-getRandomRecipe()
-    .then(data => {
-        console.log(data);
-    })
-    .catch(err => {
-        console.log(err);
-    });
