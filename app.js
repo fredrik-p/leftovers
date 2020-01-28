c3f793d006670996a0c961a670a5fbba7b99be06
Jasmine Silfver <jasmine.silfver@gmail.com>
2020-01-28 11:42:07 +0100

    Fetching from API. Again

--- app.js
+++ app.js
@@ -5,3 +5,15 @@
 
 
 // Fetching from API
+const getRandomRecipe = async () => {
+    const response = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=1bb4a5aaed654b6e9134576586d04a17`);
+    return await response.json();
+};
+
+getRandomRecipe()
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => {
+        console.log(err);
+    });
