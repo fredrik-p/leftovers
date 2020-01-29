0b42b1a1987d67b3f9fa36502556bf1d350bd71b
oscarcamp <33449861+oscarcamp@users.noreply.github.com>
2020-01-29 14:12:28 +0100

    Instructions are showed on the display

--- app.js
+++ app.js
@@ -23,7 +23,7 @@
 
         getRandomRecipe()
             .then(data => {
-                //console.log(data);
+                console.log(data);
                 //console.log(data.recipes[0].analyzedInstructions[0].steps)
                 //console.log()
                 //console.log()
@@ -43,24 +43,38 @@
                         <div class="card-body">
                             <h2 class="card-title">${data.recipes[0].title}</h2>
                             <ul class="ingredients" id="ingredientList">
-                            
                             </ul>
-                            <p class="card-text instructions"></p>
-                            <p class="card-text instructions">${data.recipes[0].servings}Servings</p>
+                            <ol class="instruction" id="instructionlist">
+                            </ol>
+                            <p class="card-text instructions">${data.recipes[0].cookingMinutes} Minuter</p>
+                            <p class="card-text instructions">${data.recipes[0].servings} Potioner</p>
 
                         </div>
                     </div>
                 </div>`;
                 document.querySelector("#recipe").innerHTML = output;
 
+                
+                //Ingredients forEach function
                 const ingredients = data.recipes[0].extendedIngredients;
 
                 ingredients.forEach((item) =>{
                     let liEl = document.createElement("LI");
                     //<li>${item.name} ${item.measures.metric.amount} ${item.measures.metric.unitShort} </li>
 
-                    liEl.innerHTML = `${ item.name } ${ item.measures.metric.amount } ${ item.measures.metric.unitShort }`;
+                    liEl.innerHTML = `${ item.name }: ${ item.measures.metric.amount } ${ item.measures.metric.unitShort }`;
                     document.querySelector("#ingredientList").append(liEl);
+                });
+
+                //Instruction forEach function
+                const instruction = data.recipes[0].analyzedInstructions[0].steps;
+
+                instruction.forEach((res) => {
+                    let liElE = document.createElement("LI");
+                    //<li>${item.name} ${item.measures.metric.amount} ${item.measures.metric.unitShort} </li>
+
+                    liElE.innerHTML = `${res.step}`;
+                    document.querySelector("#instructionlist").append(liElE);
                 });
             })
             .catch(err => {
