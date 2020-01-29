785e28b5424421a95fa7fa771e3d25af2f476149
Martin Gustavsson <Marreeeehh93@gmail.com>
2020-01-29 10:52:16 +0100

    printing out data

--- app.js
+++ app.js
@@ -5,18 +5,23 @@
 
 
 // // Fetching from API
-// const getRandomRecipe = async () => {
-//     const response = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=1bb4a5aaed654b6e9134576586d04a17`);
-//     return await response.json();
-// };
+ const getRandomRecipe = async () => {
+     const response = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=1bb4a5aaed654b6e9134576586d04a17`);
+     return await response.json();
+ };
 
-// getRandomRecipe()
-//     .then(data => {
-//         console.log(data);
-//     })
-//     .catch(err => {
-//         console.log(err);
-//     });
+ getRandomRecipe()
+     .then(data => {
+         console.log(data);
+         console.log(data.recipes[0].analyzedInstructions[0].steps)
+         console.log(data.recipes[0].title)
+         console.log(data.recipes[0].image)
+         console.log(data.recipes[0].cookingMinutes)
+         console.log(data.recipes[0].servings)                       
+     })
+     .catch(err => {
+         console.log(err);
+     });
 
 
 
