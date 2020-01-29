098bb533f123aab3358735ca58d35dc7f365c754
oscarcamp <33449861+oscarcamp@users.noreply.github.com>
2020-01-29 12:11:58 +0100

    Working by Oscar

--- app.js
+++ app.js
@@ -10,20 +10,7 @@
      return await response.json();
  };
 
- getRandomRecipe()
-     .then(data => {
-         console.log(data);
-         console.log(data.recipes[0].analyzedInstructions[0].steps)
-         console.log(data.recipes[0].title)
-         console.log(data.recipes[0].image)
-         console.log(data.recipes[0].cookingMinutes)
-         console.log(data.recipes[0].servings)                     
-     })
-     .catch(err => {
-         console.log(err);
-             });
-
-
+ 
 
 
 
@@ -33,5 +20,50 @@
 
     button.addEventListener('submit', (e) => {
         e.preventDefault();
-        console.log('I´ve been clicked');
+
+        getRandomRecipe()
+            .then(data => {
+                //console.log(data);
+                //console.log(data.recipes[0].analyzedInstructions[0].steps)
+                //console.log()
+                //console.log()
+                //console.log(data.recipes[0].cookingMinutes)
+                //console.log()
+
+                
+
+                let output =
+
+                `<div class="img-body">
+                    <img src="${data.recipes[0].image}" class="card-img-top" alt="">
+            </div>
+
+                    <div class="card">
+
+                        <div class="card-body">
+                            <h2 class="card-title">${data.recipes[0].title}</h2>
+                            <ul class="ingredients" id="ingredientList">
+                            
+                            </ul>
+                            <p class="card-text instructions"></p>
+                            <p class="card-text instructions">${data.recipes[0].servings}Servings</p>
+
+                        </div>
+                    </div>
+                </div>`;
+                document.querySelector("#recipe").innerHTML = output;
+
+                const ingredients = data.recipes[0].extendedIngredients;
+
+                ingredients.forEach((item) =>{
+                    let liEl = document.createElement("LI");
+                    //<li>${item.name} ${item.measures.metric.amount} ${item.measures.metric.unitShort} </li>
+
+                    liEl.innerHTML = `${ item.name } ${ item.measures.metric.amount } ${ item.measures.metric.unitShort }`;
+                    document.querySelector("#ingredientList").append(liEl);
+                });
+            })
+            .catch(err => {
+                console.log(err);
+            });
     });
